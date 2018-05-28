c12b54d077696f25d41a68b3603b90ad8f0594ec
Ryan Phillips <rphillips4417@gmail.com>
2018-05-28 14:44:04 -0600

    Created swift file and coin class

---
+++ CryptoTracker/CoinData.swift
@@ -0,0 +1,19 @@
+//
+//  CoinData.swift
+//  CryptoTracker
+//
+//  Created by Ryan Phillips on 5/28/18.
+//  Copyright © 2018 Ryan Phillips. All rights reserved.
+//
+
+import UIKit
+
+class Coin {
+    var symbol = ""
+    var image = UIImage()
+    var price = 0.0
+    var amount = 0.0
+    var historicalData = [Double]()
+    
+    
+}
